05c85c86a8c3257fcbcdc5a772817d7dbc7866c7
AlaguSelvan <34230909+AlaguSelvan@users.noreply.github.com>
2018-07-11 09:23:52 +0530

    over

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,37 +1,42 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const RecipeSchema = new Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  imageUrl: {
+    type: String,
+    required: true
+  },
+  category: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  instructions: {
+    type: String,
+    required: true
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  likes: {
+    type: Number,
+    default: 0
+  },
+  username: {
+    type: String
+  }
+});
 
-    name: {
-        type: String,
-        require: true
-    },
+RecipeSchema.index({
+  "$**": "text"
+});
 
-    category: {
-        type: String,
-        required: true
-    },
-
-    description: {
-        type: String,
-        required: true
-    },
-    instructions: {
-        type: String,
-        required: true
-    },
-    createdDate: {
-        type: Date,
-        default: Date.now
-    },
-    likes: {
-        type: Number,
-        default: 0
-    },
-    username: {
-        type: String
-    }
-})
-
-module.exports = mongoose.model('Recipe', RecipeSchema);
+module.exports = mongoose.model("Recipe", RecipeSchema);
