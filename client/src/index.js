98c2c5f4e0e5a7040677c7cdff23c5e7aa63fc0b
alaguselvan <alaguselvan97@gmail.com>
2020-05-12 18:00:59 +0530

    fix base url

--- client/src/index.js
+++ client/src/index.js
@@ -22,7 +22,7 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4444/graphql",
+  uri: "/graphql",
   fetchOptions: {
     credentials: "include"
   },
